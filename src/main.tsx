5f80481bd8e9181142b619c0c0e2230e49264cb3
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-07-27 18:12:58 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { LoginForm } from "./components/login-form.tsx";
+import { SignUpForm } from "./components/sign-up-form.tsx";
+import { ForgotPasswordForm } from "./components/forgot-password-form.tsx";
+import { UpdatePasswordForm } from "./components/update-password-form.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+        <Route path="/forgot-password" element={<ForgotPasswordForm />} />
+        <Route path="/update-password" element={<UpdatePasswordForm />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
