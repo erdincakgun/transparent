2cfc5a82da583cd35e015c95282d468a5a7eb574
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-08-06 22:22:17 +0300

    Create 20260806192058_4.sql

---
+++ supabase/migrations/20260806192058_4.sql
@@ -0,0 +1,4 @@
+ALTER TABLE public.accounts ADD COLUMN description text;
+ALTER TABLE public.ledgers ADD COLUMN description text;
+ALTER TABLE public.organizations ADD COLUMN description text;
+ALTER TABLE public.transactions ADD COLUMN description text;
