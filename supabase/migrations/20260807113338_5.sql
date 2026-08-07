b3098001781c96a4d8d6b5519fd6eaaa2323bec5
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-08-07 14:33:51 +0300

    Create 20260807113338_5.sql

---
+++ supabase/migrations/20260807113338_5.sql
@@ -0,0 +1,4 @@
+ALTER TABLE public.accounts ADD COLUMN name text;
+ALTER TABLE public.accounts ADD COLUMN description text;
+ALTER TABLE public.ledgers ADD COLUMN name text;
+ALTER TABLE public.ledgers ADD COLUMN description text;
