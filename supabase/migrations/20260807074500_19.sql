c72426fcf9bdf026f6063ff142e3e986bd8c07bd
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-08-07 11:33:01 +0300

    Create 20260807074500_19.sql

---
+++ supabase/migrations/20260807074500_19.sql
@@ -0,0 +1,4 @@
+GRANT SELECT ON public.organizations_members TO authenticated;
+GRANT INSERT ON public.organizations_members TO authenticated;
+GRANT SELECT ON public.ledgers_members TO authenticated;
+GRANT INSERT ON public.ledgers_members TO authenticated;
