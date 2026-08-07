3b90118991159c20e0684025656faafc5b459780
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-08-07 10:20:18 +0300

    Create 20260807071948_18.sql

---
+++ supabase/migrations/20260807071948_18.sql
@@ -0,0 +1,5 @@
+CREATE POLICY "Enable insert for users based on created_by" ON public.accounts FOR INSERT TO authenticated WITH CHECK ((( SELECT auth.uid() AS uid) = created_by));
+CREATE POLICY "Enable insert for users based on created_by" ON public.ledgers FOR INSERT TO authenticated WITH CHECK ((( SELECT auth.uid() AS uid) = created_by));
+CREATE POLICY "Enable insert for users based on created_by" ON public.ledgers_members FOR INSERT TO authenticated WITH CHECK ((( SELECT auth.uid() AS uid) = created_by));
+CREATE POLICY "Enable insert for users based on created_by" ON public.organizations_members FOR INSERT TO authenticated WITH CHECK ((( SELECT auth.uid() AS uid) = created_by));
+CREATE POLICY "Enable insert for users based on created_by" ON public.transactions FOR INSERT TO authenticated WITH CHECK ((( SELECT auth.uid() AS uid) = created_by));
