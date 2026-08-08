83b235d7b56bdf01622380ad62fc03e6e8cdf1d0
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-08-08 23:36:57 +0300

    a

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar";
+import { ModeToggle } from "@/components/mode-toggle";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -40,6 +41,7 @@ export default function Dashboard() {
               </BreadcrumbList>
             </Breadcrumb>
           </div>
+          <ModeToggle></ModeToggle>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,19 @@ import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import LoginPage from "@/pages/login.tsx";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/components/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <TooltipProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </TooltipProvider>
   </StrictMode>,
 );
