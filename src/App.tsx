f5a2aa41b6323ee22c15a6c5960500cc4c888cc8
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-08-08 23:22:24 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import { useEffect } from "react";
 import { createClient } from "@/lib/supabase/client";
-import { Button } from "./components/ui/button";
-import { useNavigate } from "react-router";
+import Dashboard from "./layouts/dashboard";
 
 export default function AuthenticatedRoute() {
-  const navigate = useNavigate();
   const supabase = createClient();
-
-  const signOut = () => {
-    supabase.auth.signOut();
-    navigate("/login");
-  };
 
   useEffect(() => {
     const checkAuth = async () => {
@@ -20,18 +13,9 @@ export default function AuthenticatedRoute() {
         location.href = "/login";
       }
     };
+
     checkAuth();
   }, []);
 
-  const test = async () => {
-    const {} = await supabase.from("accounts").insert([{ name: "test" }]);
-  };
-
-  return (
-    <div>
-      Authenticated page
-      <Button onClick={test}>Test</Button>
-      <Button onClick={signOut}>Sign Out</Button>
-    </div>
-  );
+  return <Dashboard></Dashboard>;
 }
--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,8 +1,16 @@
-import { createClient as createSupabaseClient } from '@supabase/supabase-js'
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
+import { useNavigate } from "react-router";
 
 export function createClient() {
   return createSupabaseClient(
     import.meta.env.VITE_SUPABASE_URL!,
-    import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY!
-  )
+    import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY!,
+  );
+}
+
+export function signOut() {
+  const supabase = createClient();
+  const navigate = useNavigate();
+  supabase.auth.signOut();
+  navigate("/login");
 }
