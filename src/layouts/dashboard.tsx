69a89ff9f2c0cbd5da93df245a1f5973b0434c3d
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-08-10 23:42:46 +0300

    Update dashboard.tsx

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -14,6 +14,7 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { Outlet } from "react-router";
 
 export default function Dashboard() {
   return (
@@ -46,12 +47,7 @@ export default function Dashboard() {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-          </div>
-          <div className="min-h-screen flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+          <Outlet></Outlet>
         </div>
       </SidebarInset>
     </SidebarProvider>
