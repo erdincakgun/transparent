e5eb03b901f7b05521290d029a89a25e4b0f29ff
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-08-11 11:00:08 +0300

    Create account-create.tsx

---
+++ src/pages/account-create.tsx
@@ -0,0 +1,11 @@
+import { AccountCreateForm } from "@/components/account-create-form";
+
+export default function AccountCreatePage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <AccountCreateForm />
+      </div>
+    </div>
+  );
+}
