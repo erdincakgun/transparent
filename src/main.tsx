d2605573b917812803ee636beba3b8663013b69b
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-08-11 10:10:00 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import LoginPage from "@/pages/login.tsx";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -9,6 +8,10 @@ import { ThemeProvider } from "@/components/theme-provider";
 import LedgerCreatePage from "@/pages/ledger-create.tsx";
 import { RequireAuth } from "./components/require-auth.tsx";
 import Dashboard from "./layouts/dashboard.tsx";
+import SummaryPage from "./pages/summary.tsx";
+import TransactionsPage from "./pages/transactions.tsx";
+import AccountsPage from "./pages/accounts.tsx";
+import UsersPage from "./pages/users.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -19,7 +22,10 @@ createRoot(document.getElementById("root")!).render(
             <Route path="/login" element={<LoginPage />} />
             <Route element={<RequireAuth />}>
               <Route element={<Dashboard />}>
-                <Route path="/" element={<App />} />
+                <Route path="/summary" element={<SummaryPage />} />
+                <Route path="/transactions" element={<TransactionsPage />} />
+                <Route path="/accounts" element={<AccountsPage />} />
+                <Route path="/users" element={<UsersPage />} />
               </Route>
               <Route path="/ledger-create" element={<LedgerCreatePage />} />
             </Route>
