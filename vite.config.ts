1ae65a0ae17dc71e302ac0c06d184f375f7517ce
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-08-18 23:29:46 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import babel from '@rolldown/plugin-babel'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
