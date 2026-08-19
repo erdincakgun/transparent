5afabc799de18764ea7972fe474033965a4384c6
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-08-19 20:31:54 +0300

    Update transaction-create.tsx

--- src/pages/transaction-create.tsx
+++ src/pages/transaction-create.tsx
@@ -2,7 +2,7 @@ import { TransactionCreateForm } from "@/components/transaction-create-form";
 
 export default function TransactionCreatePage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-4 sm:p-6 md:p-10">
       <div className="w-full max-w-sm">
         <TransactionCreateForm />
       </div>
